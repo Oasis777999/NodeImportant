c365c2f508c4a7b43568a34a6193eb2173311527
Oasis777999 <collegeoasis915@gmail.com>
2025-07-17 13:02:05 +0530

    Middleware in node.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const sendMail = require("./sendMail");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
+const autoMiddelware = (req, res, next) => {
+  const token = req.headers.authrization;
+
+  if (!token || token !== "valid-token") {
+    return res.status(401).send("Un Authourized User");
+  }
+  next();
+};
+
+app.get("/", autoMiddelware, (req, res) => {
   res.send("Application is working");
 });
-
-app.get("/mail", sendMail);
 
 app.listen(port, () => console.log("application is running on port : ", port));
